2ef894b47fef340cd9037227f19e9058c7328b2d
sekhar <munisekharudavalapati1@gmail.com>
2024-06-20 21:23:21 +0530

    Simple Group Chat Application

--- routers/admin.js
+++ routers/admin.js
@@ -1,13 +1,27 @@
 const express=require('express');
 const router=express.Router();
+const fs=require('fs')
 
-router.get('/add-product',(req,res,next)=>{
-    res.send(`<form action="/products" method="POST" placeholder="Product Title">
-                <input type="text" name="title"> <button type="submit">add</button>
+router.get('/login',(req,res,next)=>{
+    res.send(`<form action="/products" method="POST" placeholder="user name">
+                <input type="text" name="userid"> <button type="submit">login</button>
             </form>`)
 })
 router.post('/products',(req,res,next)=>{
-    console.log(req.body)
+   
+    const id=req.body.userid
+    console.log(id);
+    localStorage.setItem();
+    res.redirect('/chat');
 })
+router.get('/chat',(req,res,next)=>{
+ res.send(`<form action="/chat" method="POST" placeholder="masage">
+                <input type="text" name="masge"> <button type="submit">send</button>
+            </form>`)
+  
+})
+router.post('/chat',(req,res,next)=>{
+     fs.writeFile('masage.text','hello')
+   })
 
 module.exports=router;
