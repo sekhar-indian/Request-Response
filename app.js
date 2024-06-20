b64c32ad7c616db5205ebb4161865c56ac257c17
sekhar <munisekharudavalapati1@gmail.com>
2024-06-20 20:20:36 +0530

    Intro to Middlewares

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express=require('express');
 const app=express();
+const bodyParser = require('body-parser');
 
-app.use((req,res,next)=>{
-    res.send('ok')
-    next();
-});
-
+// Middleware to parse URL-encoded data from forms
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/add-product',(req,res,next)=>{
+    res.send(`<form action="/products" method="POST" placeholder="Product Title">
+                <input type="text" name="title"> <button type="submit">add</button>
+            </form>`)
+})
+app.use('/products',(req,res,next)=>{
+    console.log(req.body)
+})
 
 
 app.listen(3000)
